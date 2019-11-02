fd840220debfb03163e5be4a7844c052de8c5b9f
iainc <iaincoop@gmail.com>
2019-11-02 17:36:06 +0000

    More namegen

--- grimoire.py
+++ grimoire.py
@@ -21,9 +21,11 @@ def generate_subtitle():
     subtitle = random.choice(first_words) + " " + random.choice(artes_adj) + " " + random.choice(artes_noun) + " " + random.choice(artes_verb) + " " + random.choice(spirits) + " " + random.choice(final)
     return subtitle
 
-def name():
-    first_syl = ["Az", "Nab", "Mor", "Ai", "Bath", "Beth", "Sal", "Eli", "Bot", "Gus", "Bu", "Ler", "Bar" "Ab", "Arg"]
-    middle_syl = ["az", "mor", "oth", "al", "ell", "el", "all", "an"]
+def name(names_list):
+    first_syl = ["A", "Na", "Mo", "Ai", "Ba", "Be", "Sa", "E", "Bo", "Gu", "Bu", "Le", "Ba" , "Ar"]
+    cons = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "l", "m", "n", "p", "q", "r", "s", "t", "th", "v", "w", "x", "y", "z"]
+    vowel = ["a", "e", "i", "o", "u", "y"]
+    middle_syl = ["az", "mor", "oth", "al", "el", "an"]
     last_syl = ["oth", "eros", "eos", "os", "ar", "son", "mon", "der", "aye", "oin", "er", "alam", "ael", "res", "iel"]
     roll = random.randint(0,99)
     """
@@ -31,13 +33,44 @@ def name():
     10-94: Reasonable name
     95-99: Unreasonable name
     """
-    print(roll)
+    name = ""
+    unique = False
+    while not unique:
+        if roll < 10: #Just to test for now
+            flip = random.randint(0,3)
+            if flip == 0:
+                name = random.choice(first_syl) + random.choice(cons)
+            else:
+                name = random.choice(first_syl) + random.choice(cons) + random.choice(last_syl)
+        elif roll < 95:
+            syllables = random.randint(1,3)
+            name += random.choice(first_syl) + random.choice(cons)
+            while syllables > 0:
+                syllable = random.choice(middle_syl)
+                name += syllable
+                syllables -= 1
+        else:
+            repeats = random.randint(3,7)
+            name = random.choice(first_syl) + random.choice(cons)
+            repeat_syl = random.choice(cons) + random.choice(vowel)
+            while repeats > 0:
+                name += repeat_syl
+                repeats -= 1
+            flip = random.randint(0,1)
+            if flip > 0:
+                name += random.choice(cons)
+        if not name in names_list:
+            unique = True
+    names_list.append(name)
+    return name
 
 #Script begins
+random.seed()
+
 title = "Grimoire"
 subtitle = generate_subtitle()
+
 body_content = "<p>Hello world!</p>"
+
 total_html_string = generate_total_page(title, subtitle, body_content)
-random.seed()
-name()
 output_final_html(total_html_string)
